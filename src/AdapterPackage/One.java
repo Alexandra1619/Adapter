bfcbce2d0a1a2badc048473f6db977aac9fa5769
Александра Медведева <Sa-medved@yandex.ru>
2021-02-09 12:30:22 +0700

    Adapter

--- src/AdapterPackage/One.java
+++ src/AdapterPackage/One.java
@@ -1,4 +1,8 @@
 package AdapterPackage;
 
 public class One {
+    public static void main(String[] args) {
+        USB cardReader = new CardReader(new MemoryCard());
+        cardReader.connectWithUsbCable();
+    }
 }
---
+++ src/AdapterPackage/USB.java
@@ -0,0 +1,5 @@
+package AdapterPackage;
+
+public interface USB {
+    void connectWithUsbCable();
+}
